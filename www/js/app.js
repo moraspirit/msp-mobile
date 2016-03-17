9a70f7798035c490b95a472b5e1eaf814a9ab769
Darshana Waasala <darshanawaasala@gmail.com>
2016-03-16 23:07:12 -0700

    simple canges

--- www/js/app.js
+++ www/js/app.js
@@ -68,5 +68,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/slug');
+  $urlRouterProvider.otherwise('/app/slug ');
 });
