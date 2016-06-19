7e4b3116b1865f4a54f0678ccd98c40a174df42f
Sasitha Sonnadara <sasithawathmal@gmail.com>
2016-06-19 15:09:37 +0530

    Modified styles on Articles page

--- server/server.js
+++ server/server.js
@@ -134,6 +134,6 @@ app.get('/albumsMore/:articleOffset', function (req, res) {
 
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
   console.log('Example app listening on port 3000!');
 });
