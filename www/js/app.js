fedd2720f54a39957d1520c26a7b365f9de10cfa
Sasitha Sonnadara <sasithawathmal@gmail.com>
2016-06-23 21:06:41 +0530

    Merge branch 'develop' of https://github.com/moraspirit/msp-mobile into develop
    Conflicts:
    	www/templates/menu.html

--- www/js/app.js
+++ www/js/app.js
@@ -147,5 +147,5 @@ angular.module('starter',
         }
       });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/about');
+    $urlRouterProvider.otherwise('/app/articles');
   });
