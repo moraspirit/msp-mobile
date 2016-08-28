aad22d3f5783f88ba743dea8e639df5765a64def
Malith <malit.tilak@gmail.com>
2016-08-28 12:55:10 +0530

    Add logging

--- server/server.js
+++ server/server.js
@@ -9,17 +9,20 @@ var cors = require('cors');
 var https = require("https");
 var myParser = require("body-parser");
 var gcm = require('node-gcm');
-
-//import the mongodb native drivers.
+const winston = require('winston')
 var mongodb = require('mongodb');
-//"MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
-// Connection URL. (This is where your mongodb server is running)
+
+
+// constants  TODO: move these to a configuration file???
 var MONGO_URL = 'mongodb://mspmalith:123123@ds153765.mlab.com:53765/moraspiritpush';
 var GCM_API_KEY = "AIzaSyCKWUYkrXOfPXTCtw6eJvlNh-n2WZw7fOk";
 var FB_ACCESS_TOKEN = 'EAAOoAjs48vsBAEDsCtu0AsStZAveePZCXaJZB8NXTFdwFrAjRYodyJ828TwAaZABen2ZB3G38oEiyExGczNSByijxjpUBLmAZCXRzsQxUPhd2KeWrSEd2SwdMqK87xxRDDMlY1IzeNtXRtAf4HlH3SoHmZAxcWjVFUZD';
-FB.setAccessToken(FB_ACCESS_TOKEN);
+const PORT_NO = 3000;
 
+FB.setAccessToken(FB_ACCESS_TOKEN);
+var MongoClient = mongodb.MongoClient;
+
+// TODO: credintials???
 var pool = mysql.createPool({
   host: 'moraspirit.com',
   user: 'morasp5_slug2016',
@@ -34,49 +37,27 @@ app.use(cors());
 app.use(myParser.json());
 app.use(myParser.urlencoded({extended: true}));
 
-// TODO: comment the API methods about what is returning
-
-/**
- * remove this!!!
- */
-app.get('/', function (req, res) {
-  var query = 'SELECT n.nid, n.title, u.name, n.created, fdb.body_summary, nfi.uri FROM (SELECT * from `msp_node_revision` NATURAL JOIN `msp_node` ) n LEFT JOIN `msp_field_data_body` fdb ON fdb.entity_id = n.nid LEFT JOIN (SELECT * FROM (SELECT entity_id, `field_featured_article_image_fid` AS fidd FROM `msp_field_data_field_featured_article_image` UNION SELECT entity_id, `field_sports_image_fid` AS fidd FROM `msp_field_revision_field_sports_image`) AS fi LEFT JOIN `msp_file_managed` fm ON fi.fidd = fm.fid) nfi ON nfi.entity_id = n.nid LEFT JOIN `msp_users` u ON n.uid= u.uid ORDER BY n.created DESC  LIMIT 10 ';
-
-  // using connection pool
-  pool.getConnection(function (err, connection) {
-    if (err) {
-      res.status(500).json(err);
-    }
-    connection.query(query, function (err, rows) {
-      // release connection to pool
-      connection.release();
-      if (err) {
-        res.status(500).json(err);
-      }
-      else {
-        res.json(rows);
-      }
-    });
-  });
-
-//optimized query  SELECT n.nid, n.title, u.name, n.created, fdb.body_summary, nfi.uri FROM (SELECT * from (select nid, title, created from `msp_node` ORDER BY created DESC LIMIT 10) as a  NATURAL JOIN `msp_node_revision`  ) n LEFT JOIN `msp_field_data_body` fdb ON fdb.entity_id = n.nid LEFT JOIN (SELECT * FROM (SELECT entity_id, `field_featured_article_image_fid` AS fidd FROM `msp_field_data_field_featured_article_image` UNION SELECT entity_id, `field_sports_image_fid` AS fidd FROM `msp_field_revision_field_sports_image`) AS fi LEFT JOIN `msp_file_managed` fm ON fi.fidd = fm.fid) nfi ON nfi.entity_id = n.nid LEFT JOIN `msp_users` u ON n.uid= u.uid ;
-
-
-});
-
 /**
  * Returns the newest 10 articles
+ * nid
+ * title
+ * publisher
+ * created time stamp
+ * body summary
+ * cover image URL
  */
 app.get('/articles', function (req, res) {
 
   var query = 'SELECT n.nid, n.title, u.name, n.created, fdb.body_summary, nfi.uri FROM (SELECT uid, nid, title, created  from  (SELECT nid, title, created FROM `msp_node` where type=\'featured_article\' or type=\'sports_article\' ORDER BY created DESC  LIMIT 10)  a NATURAL JOIN `msp_node_revision`  b ) n LEFT JOIN `msp_field_data_body` fdb ON fdb.entity_id = n.nid LEFT JOIN (SELECT * FROM (SELECT entity_id, `field_featured_article_image_fid` AS fidd FROM `msp_field_data_field_featured_article_image` UNION SELECT entity_id, `field_sports_image_fid` AS fidd FROM `msp_field_revision_field_sports_image`) AS fi LEFT JOIN `msp_file_managed` fm ON fi.fidd = fm.fid) nfi ON nfi.entity_id = n.nid LEFT JOIN `msp_users` u ON n.uid= u.uid';
   pool.getConnection(function (err, connection) {
     if (err) {
+      winston.log('error', "Error while getting connection from the pool "+ err);
       res.status(500).json(err);
     }
     connection.query(query, function (err, rows, fields) {
       connection.release();
       if (err) {
+        winston.log('error', "Error while querying moraSpirit main DB "+ err);
         res.status(500).json(err);
       }
       else {
@@ -95,11 +76,13 @@ app.get('/articlesMore/:articleOffset', function (req, res) {
   var query = "SELECT n.nid, n.title, u.name, n.created, fdb.body_summary, nfi.uri FROM (SELECT uid, nid, title, created  from  (SELECT nid, title, created FROM `msp_node` where type=\'featured_article\' or type=\'sports_article\' ORDER BY created DESC  LIMIT 10 OFFSET ? )  a NATURAL JOIN `msp_node_revision`  b ) n LEFT JOIN `msp_field_data_body` fdb ON fdb.entity_id = n.nid LEFT JOIN (SELECT * FROM (SELECT entity_id, `field_featured_article_image_fid` AS fidd FROM `msp_field_data_field_featured_article_image` UNION SELECT entity_id, `field_sports_image_fid` AS fidd FROM `msp_field_revision_field_sports_image`) AS fi LEFT JOIN `msp_file_managed` fm ON fi.fidd = fm.fid) nfi ON nfi.entity_id = n.nid LEFT JOIN `msp_users` u ON n.uid= u.uid";
   pool.getConnection(function (err, connection) {
     if (err) {
+      winston.log('error', "Error while getting connection from the pool "+ err);
       res.status(500).json(err);
     }
     connection.query(query, [articleOffset], function (err, rows, fields) {
       connection.release();
       if (err) {
+        winston.log('error', "Error while querying moraSpirit main DB "+ err);
         res.status(500).json(err);
       }
       else {
@@ -111,6 +94,12 @@ app.get('/articlesMore/:articleOffset', function (req, res) {
 
 /**
  * Returns single article for the passed ID
+ * title
+ * publisher
+ * created date
+ * modified date
+ * article body
+ * cover image URL
  */
 app.get('/articles/:articleId', function (req, res) {
 
@@ -118,11 +107,13 @@ app.get('/articles/:articleId', function (req, res) {
   var query = "SELECT  n.title, u.name,FROM_UNIXTIME(n.created,' %D %M %Y %h:%i:%p ') as created, FROM_UNIXTIME(n.timestamp,'%Y %D %M %h:%i:%p') as modified, fdb.body_value, nfi.uri FROM (SELECT uid, nid, title, created, timestamp  from  (SELECT nid, title, created FROM `msp_node` where nid = " + articleId + ")  a NATURAL JOIN `msp_node_revision`  b ) n LEFT JOIN `msp_field_data_body` fdb ON fdb.entity_id = n.nid LEFT JOIN (SELECT * FROM (SELECT entity_id, `field_featured_article_image_fid` AS fidd FROM `msp_field_data_field_featured_article_image` UNION SELECT entity_id, `field_sports_image_fid` AS fidd FROM `msp_field_revision_field_sports_image`) AS fi LEFT JOIN `msp_file_managed` fm ON fi.fidd = fm.fid) nfi ON nfi.entity_id = n.nid LEFT JOIN `msp_users` u ON n.uid= u.uid ;";
   pool.getConnection(function (err, connection) {
     if (err) {
+      winston.log('error', "Error while getting connection from the pool "+ err);
       res.status(500).json(err);
     }
     connection.query(query, function (err, rows, fields) {
       connection.release();
       if (err) {
+        winston.log('error', "Error while querying moraSpirit main DB "+ err);
         res.status(500).json(err);
       }
       else {
@@ -133,16 +124,17 @@ app.get('/articles/:articleId', function (req, res) {
 });
 
 /**
- * Return 10 facebook albums per request with following data
- * -album name
- * -cover photo  (id, created time)
- * -likes (count)
- * -comments (count)
+ * Return 10 facebook albums per request
+ * album name
+ * cover photo  (id, created time)
+ * likes (count)
+ * comments (count)
  */
 app.get('/albums', function (req, res) {
   FB.api('moraspirit.fanpage/albums?fields=name,cover_photo,likes.limit(0).summary(true),comments.limit(0).summary(true)&limit=10', function (data) {
     if (!data || data.error) {
       console.log(!res ? 'error occurred' : res.error);  // TODO: add LOG....!
+      winston.log('error', 'Error occurred :' + res.error);
       res.status(500).json(data);
     }
     else {
@@ -180,19 +172,18 @@ app.post('/push', function (req, res) {
 
   MongoClient.connect(MONGO_URL, function (err, db) {
     if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);  // TODO: LOG this
+      winston.log('error', 'Unable to connect to the mongoDB server. Error:'+ err);
     } else {
-      console.log('Connection established to', MONGO_URL);   // TODO: LOG this
+      winston.log('info','Connection established to' +  MONGO_URL );
       var collection = db.collection('device_tokens');
       collection.find({}).toArray(function (err, result) {
         if (err) {
-          console.log(err);   // TODO: LOG this
+          winston.log('error', err);
         } else if (result.length) {
           var tokens = [];
           result.forEach(function (document) {
             tokens.push(document.token);
           });
-          console.log(tokens);   // TODO: LOG this
           var service = new gcm.Sender(GCM_API_KEY);
           var message = new gcm.Message();
           message.addData('title', title);
@@ -214,16 +205,16 @@ app.post('/push', function (req, res) {
 
           service.sendNoRetry(message, {registrationTokens: tokens}, function (err, response) {
             if (err) {
-              console.log('problem with request: ' + err);
+              winston.log('error', 'problem with request: ' + err);
             }
             else {
-              console.log("Notification sent successfully!");  // TODO: LOG this
-              console.log('response: ' + JSON.stringify(response));  // TODO: LOG this
-              res.send("success");
+              winston.log('info', "Notification sent successfully!");
+              winston.log('info', 'Response from GCM: ' + JSON.stringify(response));
+              res.send("success"); // TODO: refactor this!
             }
           });
         } else {
-          console.log('No devices are registered yet!!');  // TODO: LOG this
+          winston.log('warn','No devices are registered yet!!');
         }
       });
       db.close();
@@ -236,26 +227,26 @@ app.post('/push', function (req, res) {
  */
 app.post('/saveDeviceToken', function (req, res) {
   var token = req.body.token;
-  console.log(token);  // TODO: log this
+  winston.log('silly', 'Device is requesting to save device token: '+ token);
 
   MongoClient.connect(MONGO_URL, function (err, db) {
     if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);   // TODO: log this
+      winston.log('error', 'Unable to connect to the mongoDB server. Error:'+ err);
     } else {
-      console.log('Connection established to', MONGO_URL);   // TODO: log this
+      winston.log('info', 'Connection established to'+ MONGO_URL);
       var collection = db.collection('device_tokens');
       var deviceTokenJSONObject = {token: token};
       collection.updateOne({token: token}, deviceTokenJSONObject, {upsert: true}, function (err, result) {
         if (err) {
-          console.log("Error while saving device token in db" + err);  // TODO: log this
-          res.send("error");
+          winston.log('error', "Error while saving device token in db" + err);
+          res.send("error");  // TODO: refactor this!
         } else if (result.modifiedCount == 1) {
-          console.log('The device is already registered');   // TODO: log this
-          res.send("success");
+          winston.log('warn', 'The device is already registered');
+          res.send("success"); // TODO: refactor this!
         }
         else {
-          console.log('Successfully saved new device token in db'); // TODO: log this
-          res.send("success");
+          winston.log('info', 'Successfully saved new device token in db');
+          res.send("success"); // TODO: refactor this!
         }
       });
       db.close();
@@ -264,6 +255,6 @@ app.post('/saveDeviceToken', function (req, res) {
 });
 
 // TODO: This port should be changed (MORA can't request 3000!!!
-app.listen(3000, function () {
-  console.log('MoraSpirit Mobile APP API is listening on port 3000!');   // TODO: log this
+app.listen(PORT_NO, function () {
+  winston.log('info','MoraSpirit Mobile APP API is listening on port !'+ PORT_NO);   // TODO: log this
 });
